40d9b59aca160024cee41d0be5e694d1b55d5f9c
spiral <spiral@spiral.sh>
2021-12-18 16:38:13 -0500

    add hrpc.net chat bot example (#1)

---
+++ chat/bot/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Net.WebSockets;
+using Chat;
+
+using Generated;
+
+namespace bot;
+
+public static class Program
+{
+    public static ChatClient client = new("http://localhost:2289");
+
+    public static async Task Main(string[] args)
+    {
+        var stream = await client.StreamMessages(new Empty());
+
+        while (stream.State == WebSocketState.Open)
+        {
+            var message = await stream.Read();
+            _ = HandleMessageReceived(message);
+        }
+
+        Console.WriteLine($"Stream client closed with status {stream.CloseStatus}, exiting...");
+    }
+
+    public static async Task HandleMessageReceived(Message message)
+    {
+        try
+        {
+            Console.WriteLine($"Message received: {message.Content}");
+            if (message.Content == "!ping")
+                await client.SendMessage(new Message() { Content = "Pong!" });
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Error in handle message received: {e}");
+        }
+    }
+}
